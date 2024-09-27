4efafdf201457d79d19ccbbd88a59596a677b182
Gustavonn07 <gustavonepnog@gmail.com>
2024-09-27 12:41:34 -0300

    add: adicionando configs vite, radix/shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Button } from './components'
 import './index.css'
 
 export const App = () => {
 
   return (
-    <div>App</div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <Button
+        variant={'destructive'}
+        onClick={() => {
+          window.alert('Olá mundo!')
+        }}
+      >
+        Clique em mim!
+      </Button>
+    </div>
   )
 }
 
